1d380636582345330bae981e2c7cc94148e55744
PushNik <31669237+NikolaPushkin@users.noreply.github.com>
2017-12-14 00:14:29 +0300

    Create ViewMetaDataInBrowser_cs.js

---
+++ ViewMetaDataInBrowser_cs.js
@@ -0,0 +1,5 @@
+<script src="http://code.jquery.com/jquery-3.2.1.js"></script>
+<script>
+console.log('%c%s', 'color: #444645; background-color:#c9e2ce;font-weight:bold;font-size:18px;','DESCRIPTION: '+jQuery('meta[name="description"]').attr('content'));
+console.log('%c%s', 'color: #444645; background-color:#c9e2ce;font-weight:bold;font-size:18px;','KEYWORDS: '+jQuery('meta[name="keywords"]').attr('content'));
+</script>
